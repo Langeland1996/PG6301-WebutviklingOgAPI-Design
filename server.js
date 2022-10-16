10ab9eaa84bbc212be01dd11e536ca4fe316cb20
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-10-16 09:46:42 +0200

    Respond to get request, added nodemon server.js to scripts so server updated live from coding changes

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/login", (req, res) => {
+    res.json({
+        username: "admssin"
+    });
+})
+
 //Tells the server to listen to port 3000
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`server started at http://localhost:${server.address().port}`)
