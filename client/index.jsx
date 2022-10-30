4f1a56b2a6b4e6eb069ab31116a932065b03ad92
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-10-30 09:39:04 +0100

    Ready for deployment

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>NiHAAAAAAAAAAO</h1>, document.getElementById("app"));
+function Frontpage() {
+  return (
+    <div>
+      <h1>Welcome to the frontpage</h1>
+    </div>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Frontpage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ app.get("/api/login", (req, res) => {
   });
 });
 
+app.use(express.static("../client/dist"));
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 });
