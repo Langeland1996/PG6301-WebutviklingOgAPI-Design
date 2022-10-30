54331114f633e29aa3c4fdcbe8838aa52d6b88b3
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-10-30 10:11:17 +0100

    Load user information from server to FrontPage()

--- client/index.jsx
+++ client/index.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-function Frontpage() {
-  return (
-    <div>
-      <h1>Welcome to the frontpage</h1>
+class LoginLinks extends React.Component {
+  render() {
+    return (
       <div>
         <Link to={"/login"}>Login</Link>
         <br></br>
         <Link to={"/register"}>Register new user</Link>
       </div>
+    );
+  }
+}
+
+function Frontpage() {
+  const [user, setUser] = useState();
+
+  useEffect(async () => {
+    const res = await fetch("/api/login");
+    setUser(await res.json());
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to the frontpage</h1>
+      {user ? <div>{user.fullName}</div> : <LoginLinks />}
     </div>
   );
 }
