ae9f53c44e2e0a09fa17ee98a8fbe16a869b0e66
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-10-31 12:56:41 +0100

    Use react-router

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Homepage</h1>, document.getElementById("app"));
+function AddNewMovie() {
+  return (
+    <div>
+      <h1>Add new movie</h1>
+    </div>
+  );
+}
+
+function ListMovies() {
+  return (
+    <div>
+      <h1>List movies</h1>
+    </div>
+  );
+}
+
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movie Database</h1>
+      <div>
+        <Link to={"/movies"}>List movies</Link>
+        <br></br>
+        <Link to={"/movies/new"}>Add new movies</Link>
+      </div>
+    </div>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/movies"} element={<ListMovies />} />
+        <Route path={"/movies/new"} element={<AddNewMovie />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
